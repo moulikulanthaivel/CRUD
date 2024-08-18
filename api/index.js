5a6ace8a61dce81a895c1f2a620c507cc408fa73
mouli.k <134676928+moulikulanthaivel@users.noreply.github.com>
2024-08-18 23:35:34 +0530

    ysy

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 import  express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config({path:"./setting/.env"})
+let port  = process.env.PORT
+let host  = process.env.HOST
+let mongo  = process.env.MONGO_URL
+
+mongoose.connect(mongo)
+.then(()=>{console.log('db connected')})
+.catch((err)=>{console.log('db not connected')})
+
 
 const app = express();
 
